4b0114a3bf04dd63dfb95da13d2ee03ce7b28445
M-elhamzawy <m.elhamzawy12@gmail.com>
2023-09-08 18:41:15 -0700

    print lower qand upper alpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main -print upper and lower alpha
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	char letter;
+	char L;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	for (L = 'A'; L <= 'Z'; L++)
+		putchar(L);
+	putchar("\n")
+	return (0);
+}
