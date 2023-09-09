98408c78412fd0f73429d84bd11c53639057d218
M-elhamzawy <m.elhamzawy12@gmail.com>
2023-09-08 18:51:21 -0700

    print all alpha except q and o

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main -print upper and lower alpha
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+        char letter;
+
+        for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter == 'q' || letter == 'e')
+			letter++;
+		else
+			putchar(letter);
+	}
+	putchar('\n');
+        return (0);
+}
