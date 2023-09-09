ef37fc8bfb8b4cc86cfe56b28f826569dfa6256f
M-elhamzawy <m.elhamzawy12@gmail.com>
2023-09-08 19:31:33 -0700

    print all base 16 paramters

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints hexa decimal base
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char letter;
+	int a;
+
+	for (a = 0; a < 10; a++)
+		putchar('0' +i);
+	for (letter = 'a'; letter >= 'f'; letter++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
