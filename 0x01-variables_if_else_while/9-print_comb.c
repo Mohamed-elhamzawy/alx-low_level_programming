9721983424c4fc3daa6235bb0041ebc9f6f44cfc
M-elhamzawy <m.elhamzawy12@gmail.com>
2023-09-08 19:59:50 -0700

    all available combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,12 +3,12 @@
 /**
  * main - Prints all possible combinations 
  *
- * Return 0.
- */
+ * Return: Always 0.
+*/
 int main(void)
 {
 	int a;
-
+	
 	for (a = 0; a <= 9; a++)
 	{
 		putchar((a % 10) + '0');
