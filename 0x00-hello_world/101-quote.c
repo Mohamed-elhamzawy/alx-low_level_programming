7ceb2c37d6fb51e58361e890c7f2d8de5a7d6d79
M-elhamzawy <m.elhamzawy12@gmail.com>
2023-09-08 20:55:38 -0700

    confusing

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ *  main - entry point
+ *
+ *  description: print a text without using printf or puts
+ *
+ *  Return: Always 0
+*/
+int main(void)
+{
+	char txt[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	for (int i = 0; txt[i] != '\0'; i==)
+		putchar(txt[i]);
+	putchar('\n');
+	return (0);
+}	
