c5bc5e228c8670b0d902b5690928a09d70b95a8b
M-elhamzawy <m.elhamzawy12@gmail.com>
2023-09-08 19:02:32 -0700

    print all alpha except q and o

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,13 +9,11 @@ int main(void)
 {
 	char letter;
 
-       	for (letter = 'a'; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-	if (letter == 'q' || letter == 'e')
-		letter++;
-	else
-		putchar(letter);
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
 	}
 	putchar('\n');
-        return (0);
+	return (0);
 }
