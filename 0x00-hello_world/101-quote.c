02064c50c926df0bdb27f0aed52fb936ea3400d5
M-elhamzawy <m.elhamzawy12@gmail.com>
2023-09-11 19:55:05 -0700

    print a whole text by putchar

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-/*
+/**
  *  main - entry point
  *
  *  description: print a text without using printf or puts
@@ -10,10 +10,10 @@
 int main(void)
 {
 	int i;
-
 	char txt[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
 	for (i = 0; txt[i] != '\0'; i++)
 		putchar(txt[i]);
 	putchar('\n');
 	return (1);
-}	
+}
