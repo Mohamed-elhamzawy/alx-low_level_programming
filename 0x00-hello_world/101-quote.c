72e161a767509aa55bf6db4dd445c0246fc16f54
M-elhamzawy <m.elhamzawy12@gmail.com>
2023-09-11 19:46:14 -0700

    print a whole text by putchar

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@
 int main(void)
 {
 	char txt[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	for (int i = 0; txt[i] != '\0'; i==)
+	for (int i = 0; txt[i] != '\0'; i++)
 		putchar(txt[i]);
 	putchar('\n');
 	return (0);
