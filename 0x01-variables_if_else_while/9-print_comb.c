c8e91d705866da996ac349225f1cabe3bd94774b
M-elhamzawy <m.elhamzawy12@gmail.com>
2023-09-11 20:15:47 -0700

    possible combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations 
+ * main - print possible combinations
  *
  * Return: Always 0.
-*/
+ */
 int main(void)
 {
 	int a;
-	
+
 	for (a = 0; a <= 9; a++)
-	{
 		putchar((a % 10) + '0');
 		if (a == 9)
 			continue;
 		putchar(',');
 		putchar(' ');
-	}
 	putchar('\n');
 	return (0);
 }
