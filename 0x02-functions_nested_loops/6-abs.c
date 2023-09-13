96dbf95bfbba954505432c956c7644b2f408fabb
M-elhamzawy <m.elhamzawy12@gmail.com>
2023-09-12 19:59:49 -0700

    m

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_sign - funtion to chech the signs
+ *
+ * @n: takes integer type input
+ *
+ * Return: return 1 if its +ve and 0 if zero and -1 if its -ve
+ */
+
+int _abs(int)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
+}
