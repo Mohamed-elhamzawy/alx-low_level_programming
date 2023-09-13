7ffefeac48149071e2c769aa069e1c5199ab4f9c
M-elhamzawy <m.elhamzawy12@gmail.com>
2023-09-12 19:28:44 -0700

    check for input is lower case or not

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _islower - function to chech if
- *            character lower case
+ * _islower - function to check if
+ *            character is lower case
  *
- * @C: checks input for function
+ * @c: checks input for function
  *
  * Return: returns 1 if it 'c' is lower case
  *                 otherwise always 0
