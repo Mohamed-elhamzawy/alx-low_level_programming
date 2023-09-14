7e33e90ba6c68acb760f44dde325aee23ec654ef
M-elhamzawy <m.elhamzawy12@gmail.com>
2023-09-13 19:31:27 -0700

    print 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * times_table - function print a 9 table
+ *
+ */
+
+void times_table(void)
+{
+	int rw;
+	int co;
+	int mult;
+
+	for (rw = 0; rw <= 9; rw++)
+	{
+		_putchar(48);
+		for (co = 0; co <= 9; co++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			mult = rw * co;
+
+			if(mult <= 9)
+				_putchar(' ');
+			else
+			{
+				_putchar((mult /100) + '0');
+				_putchar((mult %10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
