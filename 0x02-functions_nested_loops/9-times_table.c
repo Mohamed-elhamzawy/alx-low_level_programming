53e32e6e3d203774a8fa9e387219c648b71f8d8c
M-elhamzawy <m.elhamzawy12@gmail.com>
2023-09-13 19:35:29 -0700

    print 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,20 +14,18 @@ void times_table(void)
 	for (rw = 0; rw <= 9; rw++)
 	{
 		_putchar(48);
-		for (co = 0; co <= 9; co++)
+		for (co = 1; co <= 9; co++)
 		{
 			_putchar(',');
 			_putchar(' ');
 
 			mult = rw * co;
 
-			if(mult <= 9)
+			if (mult <= 9)
 				_putchar(' ');
 			else
-			{
-				_putchar((mult /100) + '0');
-				_putchar((mult %10) + '0');
-			}
+				_putchar((mult / 100) + '0');
+			_putchar((mult % 10) + '0');
 		}
 		_putchar('\n');
 	}
