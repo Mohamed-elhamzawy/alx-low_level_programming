af0225e551116b349db06e4cba0e5ba238f12fb4
M-elhamzawy <m.elhamzawy12@gmail.com>
2023-09-13 18:23:48 -0700

    count the time in whole the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print every minute of jack
+ *
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	int j;
+	int k;
+	int h;
+
+	for (i = 0; i <= 2; i++)
+		_putchar(i + '0');
+		for (j = 0; j <= 3; j++)
+			_putchar(j + '0');
+			_putchar(58);
+			for (k = 0; k <= 5; k++)
+				_putchar(k + '0');
+				for (h = 0; h <= 9; h++)
+					_putchar(h + '0');
+					_putchar('\n');
+}
