31819dc4d7e6e2cea4eb10192532c54f00859549
M-elhamzawy <m.elhamzawy12@gmail.com>
2023-09-13 18:36:56 -0700

    count the time in whole the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,13 +13,22 @@ void jack_bauer(void)
 	int h;
 
 	for (i = 0; i <= 2; i++)
+	{
 		_putchar(i + '0');
 		for (j = 0; j <= 3; j++)
+		{
 			_putchar(j + '0');
 			_putchar(58);
 			for (k = 0; k <= 5; k++)
+			{
 				_putchar(k + '0');
 				for (h = 0; h <= 9; h++)
+				{
 					_putchar(h + '0');
 					_putchar('\n');
+				}
+			}
+		
+		}	
+	}
 }
