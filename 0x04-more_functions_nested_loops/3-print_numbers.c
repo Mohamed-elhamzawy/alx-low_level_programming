de3fa755a62b902b77f90de70c436904e27ddaa3
M-elhamzawy <m.elhamzawy12@gmail.com>
2023-09-14 14:38:21 -0700

    print numbers from 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - function to print number
+ *
+ */
+
+void print_numbers(void)
+{
+	char ch;
+
+	for (ch = 48; ch <= 57; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
