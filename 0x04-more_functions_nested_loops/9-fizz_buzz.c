9eb821fae5b7b31b95ef689993faea243d8132d8
M-elhamzawy <m.elhamzawy12@gmail.com>
2023-09-14 17:18:30 -0700

    fizz or buzz or nothinggg!

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - function to print numbers with fizz and fozz
+ * swparated by comma
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			if (i % 5 == 0)
+			{
+				printf("FizzBuzz ");
+			}
+			else
+			{
+				printf("Fizz ");
+			}
+		}
+		else if (i % 5 == 0)
+		{
+			if (i % 3 != 0)
+			{
+				printf("Buzz ");
+			}
+		}
+		else if (i % 3 != 0 && i % 5 != 0)
+		{
+			printf("%d", i);
+			printf(" ");
+		}
+	}
+	printf("\n");
+}
