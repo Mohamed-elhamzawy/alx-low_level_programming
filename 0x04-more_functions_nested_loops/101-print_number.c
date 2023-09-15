4e4f5043057f9b1895fa77c08d90f97639d7d600
M-elhamzawy <m.elhamzawy12@gmail.com>
2023-09-14 19:43:04 -0700

    print all integers by putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,12 +8,20 @@
 
 void print_number(int n)
 {
+	unsigned int num;
+
 	if (n < 0)
 	{
-		_putchar('-);
-		n = -n;
+		_putchar('-');
+		num = -n;
 	}
-	if ((n / 10) > 0)
-		print_number(n / 10);
-	_putchar((n % 10) + '0');
+	else
+	{
+		num = n;
+	}
+	if ((num / 10) > 0)
+	{
+		print_number(num / 10);
+	}
+	_putchar((num % 10) + '0');
 }
