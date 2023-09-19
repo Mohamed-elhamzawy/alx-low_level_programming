118350a666197f6b2b6e7f4c1e93728111f7df15
M-elhamzawy <m.elhamzawy12@gmail.com>
2023-09-18 19:29:49 -0700

    print string in reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - print the string in reverse order
+ *
+ * @s: takes a string
+ *
+ * Return: the string elemnets in reverse order
+ */
+
+void print_rev(char *s)
+{
+	int count;
+
+	for (count = 0; *s != '\0'; s++)
+	{
+		count++;
+	}
+	for (*s = count; *s >= 0; s--)
+		_putchar(0 + *s);
+	_puthar('\n');
+}
