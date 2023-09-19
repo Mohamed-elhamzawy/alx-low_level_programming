50e7a7a5b34bf65c654ed770f2d4e83ff561dfa1
M-elhamzawy <m.elhamzawy12@gmail.com>
2023-09-18 19:55:04 -0700

    print even elemnts of string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - function to print a even
+ *
+ * @str: ake a string
+ *
+ * Return: print
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if( i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
