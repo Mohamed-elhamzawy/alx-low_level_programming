f02702001e68dcab3b4d35e82a5c850ef263335b
M-elhamzawy <m.elhamzawy12@gmail.com>
2023-09-21 15:50:45 -0700

    print second half of this string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,18 +11,18 @@
 void puts_half(char *str)
 {
 	int i;
-	int len;
 
-	for (len = 0; str[len] != '\0'; len++)
+	for (i = 0; str[i] != '\0'; i++)
 		;
-	if (len / 2 == 0)
+	i++;
+	if (i / 2 == 0)
 	{
-		for (i = (len / 2); str[i] != '\0'; i++)
+		for (i = (i / 2); str[i] != '\0'; i++)
 			_putchar(str[i]);
 	}
 	else
 	{
-		for (i = ((len - 1) / 2); str[i] != '\0'; i++)
+		for (i = ((i - 1) / 2); str[i] != '\0'; i++)
 			_putchar(str[i]);
 	}
 	_putchar('\n');
