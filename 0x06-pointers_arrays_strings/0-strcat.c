d7fe0c7321dbfb5d8df4c57b5102d76f764d758a
M-elhamzawy <m.elhamzawy12@gmail.com>
2023-09-21 16:47:47 -0700

    concatinate this strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,20 +3,21 @@
 /**
  * *_strcat - function to concatinate two strings
  *
- * @*dest: first string
- * @*src: second string
+ * @dest: first string
+ * @src: second string
  *
  * Return: dest string
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int i,j;
+	int i;
+	int j;
 
 	i = 0;
 	for (i = 0; dest[i] != '\0'; i++)
 		;
 	for (j = 0; src[j]; j++)
-		dest[i]=src[j];
+		dest[i] = src[j];
 	return (dest);
 }
