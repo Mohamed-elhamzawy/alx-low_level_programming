0d52a9509b48b13f2b72e1be1cb4a6025541a3d8
M-elhamzawy <m.elhamzawy12@gmail.com>
2023-09-21 17:59:13 -0700

    copy string into another string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strncpy - function to concatinate two strings
+ *
+ * @dest: first string
+ * @src: second string
+ * @n: number of byets
+ *
+ * Return: dest string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[j];
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
