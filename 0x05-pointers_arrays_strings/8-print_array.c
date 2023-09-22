0980488d724aace0ecb7438826b1cefe562bc68f
M-elhamzawy <m.elhamzawy12@gmail.com>
2023-09-21 20:22:33 -0700

    print elemnts of array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_array - function to print elemnts of array
+ *
+ * @a: array
+ * @n: number of elemnts of array
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == (n - 1))
+		{
+			_putchar(a[i] + '0');
+		}
+		else
+		{
+			_putchar(a[i] + '0');
+		       	_putchar(32);
+			_putchar(44);
+		}
+	}
+}
