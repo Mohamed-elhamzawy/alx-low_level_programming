f600a39eafbd84061c3f7dd127752ef684055203
M-elhamzawy <m.elhamzawy12@gmail.com>
2023-09-21 18:28:29 -0700

    compare between this strings

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * *reverse_array - function to reverse  strings
+ *
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ */
+
+void reverse_array(int *a, int n)
+{
+	for (
