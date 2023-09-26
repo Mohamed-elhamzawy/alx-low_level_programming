ed9769c7a3cc6f0879c2d00db32b5e5c4b7390a9
M-elhamzawy <m.elhamzawy12@gmail.com>
2023-09-25 18:45:22 -0700

    copy the string into another string

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_memcpy - function to change the meomry contains with another array
+ *
+ * @dest: array i will copy the array to
+ * @src: array i want to add
+ * @n: number of bytes
+ *
+ * Return: the new string
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	int j;
+
+	while ( i < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	return (dest);
+}
