46684984505934016ffac7aa177b94fc81c6b803
M-elhamzawy <m.elhamzawy12@gmail.com>
2023-09-25 19:29:26 -0700

    print the length of this string

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * *_strchr - function to print the length of substring
+ *
+ * @s: string
+ * @accept: string
+ *
+ * Return: the position of this char
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	int z;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] != " ")
+		{
+			for (j = 0; accept[j] != '\0'; j++)
+			{
+				if (s[i] == accept[j])
+					z++;
+			}
+		}
+		else 
+			return (z);
+	}
+	return (z);
+}
