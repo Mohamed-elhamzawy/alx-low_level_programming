45db8036bcbe8d16092308a091d5cb9e96e7a119
M-elhamzawy <m.elhamzawy12@gmail.com>
2023-09-26 19:46:19 -0700

    print chess board 2d array

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,7 +7,7 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int i,j;
+	int i, j;
 
 	for (i = 0; i < 8; i++)
 	{
@@ -17,4 +17,4 @@ void print_chessboard(char (*a)[8])
 		}
 		_putchar('\n');
 	}
-}			
+}
