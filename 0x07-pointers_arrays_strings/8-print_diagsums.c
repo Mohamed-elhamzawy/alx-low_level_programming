afa84664c65f4bfa8591cfeb975f04dc001c5aa2
M-elhamzawy <m.elhamzawy12@gmail.com>
2023-09-26 19:45:25 -0700

    rint the sum of diagonal of this array

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagsums - function
+ *
+ * @a: string
+ * @size: size of array
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, j;
+	unsigned int s1 = 0;
+	unsigned int s2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		s1 += a[i];
+		s2 += a[size - i - 1];
+		a += size;
+	}
+	printf("%lu, ", s1);
+	printf("%lu\n", s2);
+}
