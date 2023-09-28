ebed0f7cf58a9eedd573df62f7e8575a9d2383b6
M-elhamzawy <m.elhamzawy12@gmail.com>
2023-09-28 11:10:37 -0700

    print sum of multiples of num 3 or 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - function to print sum of multiples
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	unsigned long int sum = 0;
+	int i, mul, num;
+
+	printf("enter 3 or 5 for multiplis of this number");
+	scanf("%d", &num);
+	if (num == 3 || num == 5)
+	{
+		for (i = 1; i < 342; i++)
+		{
+			mul = num * i;
+			if (mul < 1024)
+			{
+				sum += mul;
+			}
+			else
+				break;
+		}
+		printf("the sum of multiplies of %d below 1024 is %lu\n", num, sum);
+	}
+	return (0);
+}
