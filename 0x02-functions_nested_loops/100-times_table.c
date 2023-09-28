65fc4ba59f11f828671d53c22371cc03f7e27042
M-elhamzawy <m.elhamzawy12@gmail.com>
2023-09-28 10:11:16 -0700

    print any time table i want

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,7 +12,7 @@ void print_times_table(int n)
 
 	if (n <= 15 && n >= 0)
 	{
-		for (i = 0; i < n; i++)
+		for (i = 0; i <= n; i++)
 		{
 			_putchar('0');
 			for (j = 1; j <= n; j++)
@@ -25,18 +25,18 @@ void print_times_table(int n)
 					_putchar(32);
 					_putchar(' ');
 				}
-				else if (mult > 9 && mult < 100)
+				if (mult > 9 && mult < 100)
 				{
 					_putchar(32);
 					_putchar((mult / 10) + 48);
 				}
-				else if (mult >= 100)
+				if (mult >= 100)
 				{
 					_putchar((mult / 100)+ '0');
 					r = mult / 10;
 					_putchar((r % 10) + '0');
 				}
-				_putchar((mult % 10));
+				_putchar((mult % 10) + '0');
 			}
 			_putchar('\n');
 		}
