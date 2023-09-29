cc94d4963efc7baa1ea8b897a513ad75665a79ca
M-elhamzawy <m.elhamzawy12@gmail.com>
2023-09-28 18:16:25 -0700

    print string in reverse order

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -10,7 +10,7 @@ void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar(*_print_rev_recursion(s - 1));
+		_putchar(_print_rev_recursion(s - 1));
 	}
 	else
 		_print_rev_recursion(s + 1);
