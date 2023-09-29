3ca32d33efc8e4312314db7e29d4604984b0cde2
M-elhamzawy <m.elhamzawy12@gmail.com>
2023-09-28 18:34:27 -0700

    lenght of the string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - function to count the length of string
+ *
+ * @s: string
+ *
+ * Return: the lenght of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int c = 0;
+
+	if (*s != '\0')
+	{
+		c += 1;
+		_strlen_recursion(s + 1);
+	}
+	else
+		return (c);
+	_putchar('\0');
+}
